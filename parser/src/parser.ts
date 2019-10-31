bf2a2c699641a1b89e6f212e5d3f8e344625d61c
Matti Ahinko <matti@ahinko.net>
2019-10-31 20:55:43 +0200

    Add basic parser

---
+++ parser/src/parser.ts
@@ -0,0 +1,17 @@
+import { CalcParser } from '../lib/Parser'
+const fs = require('fs')
+
+export function parseFile(filePath: string) {
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  console.log(CalcParser.parse(fileContent))
+}
+
+function main(args: string[]) {
+  if (args.length < 3) {
+    console.log('Usage: parser path/to/file.msic')
+  } else {
+    console.log(parseFile(args[2]))
+  }
+}
+
+main(process.argv)
