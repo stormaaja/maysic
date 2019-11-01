16a0a1b42f66de58a7549ea0a4801cf5593249db
Matti Ahinko <matti@ahinko.net>
2019-11-01 20:31:23 +0200

    Move main to own file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { parseFile } from './parser/parser'
+
+function main(args: string[]) {
+  if (args.length < 3) {
+    console.log('Usage: parser path/to/file.msic')
+  } else {
+    console.log(parseFile(args[2]))
+  }
+}
+
+main(process.argv)
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,17 +1,7 @@
-import { Parser } from '../lib/Parser'
+import { Parser } from '../../lib/Parser'
 const fs = require('fs')
 
 export function parseFile(filePath: string) {
   const fileContent = fs.readFileSync(filePath, 'utf8')
   return Parser.parse(fileContent)
 }
-
-function main(args: string[]) {
-  if (args.length < 3) {
-    console.log('Usage: parser path/to/file.msic')
-  } else {
-    console.log(parseFile(args[2]))
-  }
-}
-
-main(process.argv)
