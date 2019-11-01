4202ecdb1a831dae6501790e07cc477959e62526
Matti Ahinko <matti@ahinko.net>
2019-11-01 21:25:20 +0200

    Use program instead

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { parseFile } from './parser'
+import { convertToAST } from './ast'
 
 function main(args: string[]) {
   if (args.length < 3) {
     console.log('Usage: parser path/to/file.msic')
   } else {
-    console.log(parseFile(args[2]))
+    const rawProgram = parseFile(args[2])
+    const program = convertToAST(rawProgram)
+    console.log(program)
   }
 }
 
