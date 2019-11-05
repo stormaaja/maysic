f5b335ac58e131d19b7921d151166772a21d2a5b
Matti Ahinko <matti@ahinko.net>
2019-11-05 21:06:03 +0200

    Add location

--- lib/Parser/program.ts
+++ lib/Parser/program.ts
@@ -1,6 +1,16 @@
+interface Location {
+  startOffset: number;
+  endOffset: number;
+  startLine: number;
+  endLine: number;
+  startColumn: number;
+  endColumn: number;
+}
+
 export interface RawASTNode {
   children: RawASTNode[];
   type: string;
+  location: { start: Location; end: Location }
 }
 
 export interface RawProgram {
