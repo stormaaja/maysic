0e05a199a812de7ddc627f820d8cfbd37b49e13e
Matti Ahinko <matti@ahinko.net>
2019-11-05 20:53:51 +0200

    Print raw AST for debugging

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ function main(args: string[]) {
     console.log('Usage: parser path/to/file.msic')
   } else {
     const rawProgram = parseFile(args[2])
+    console.log(JSON.stringify(rawProgram, null, 1))
     const program = convertToAST(rawProgram)
     console.log(program)
   }
