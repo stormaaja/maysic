5d6801c5f7eab1cee2e4e6a9deadea7b8a4f6bff
Matti Ahinko <matti@ahinko.net>
2019-11-06 21:13:53 +0200

    Handle errors

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ function main(args: string[]) {
   if (args.length < 3) {
     console.log('Usage: parser path/to/file.msic')
   } else {
-    const rawProgram = parseFile(args[2])
-    console.log(JSON.stringify(rawProgram, null, 1))
-    const program = convertToAST(rawProgram)
-    console.log(program)
+    try {
+      const rawProgram = parseFile(args[2])
+      console.log(JSON.stringify(rawProgram, null, 1))
+      const program = convertToAST(rawProgram)
+      console.log(program)
+    } catch (e) {
+      console.error(e.msg)
+      console.debug(e.stack)
+    }
   }
 }
 
