b2c7ada2c05fd448255f253e81398b3479828e0f
Matti Ahinko <matti@ahinko.net>
2019-11-17 09:24:27 +0200

    Change to use lambda

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,7 +1,7 @@
 import { parse } from "../src/parser"
 
-describe("parsing assignment", function () {
-  it("produces assignment tree", function () {
+describe("parsing assignment", () => {
+  it("produces assignment tree", () => {
     const program = parse("s = 2")
     expect(program.ast.children.length).toBe(1)
     expect(program.ast.type).toBe("block")
@@ -13,8 +13,8 @@ describe("parsing assignment", function () {
   })
 })
 
-describe("parsing function", function () {
-  it("produces function without args", function () {
+describe("parsing function", () => {
+  it("produces function without args", () => {
     const program = parse("f = (): {}")
     expect(program.ast.children.length).toBe(1)
     expect(program.ast.type).toBe("block")
@@ -23,7 +23,7 @@ describe("parsing function", function () {
     expect(assign.children[0]).toBe("f")
     expect(assign.children[1].type).toBe("function")
   })
-  it("produces function with two params", function () {
+  it("produces function with two params", () => {
     const program = parse("f = (integer i, string s): {}")
     const fn = program.ast.children[0].children[1]
     const args = fn.children[0].children
@@ -37,8 +37,8 @@ describe("parsing function", function () {
   })
 })
 
-describe("parsing function call", function () {
-  it("produces function call tree", function () {
+describe("parsing function call", () => {
+  it("produces function call tree", () => {
     const program = parse("print(\"Hello World!\")")
     const fnCall = program.ast.children[0]
     expect(fnCall.type).toBe("fnCall")
@@ -48,7 +48,7 @@ describe("parsing function call", function () {
     expect(fnCall.children[1].children[0].children[0].toString()).toBe(
       "\"Hello World!\"")
   })
-  it("produces function call with symbols", function () {
+  it("produces function call with symbols", () => {
     const program = parse("s = \"Hello World!\" i = 2 print(s, i)")
     const params = program.ast.children[2].children[1].children
     expect(params.length).toBe(2)
