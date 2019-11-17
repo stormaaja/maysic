11c840e37683e82177027d6eb068a9e694c61e32
Matti Ahinko <matti@ahinko.net>
2019-11-17 09:23:46 +0200

    Set jest filenames

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   transform: { "^.+\\.ts?$": "ts-jest" },
   testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  testRegex: "/tests/.*\\.(test|spec)?\\.ts$",
+  moduleFileExtensions: ["ts", "js"],
+  collectCoverageFrom: [
+    "src/**/{!(ignore-me),}.(js|ts)",
+    "lib/**/{!(yyparse.js),}.(js|ts)"
+  ]
 }
