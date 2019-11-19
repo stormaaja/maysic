28f5b96ceffd2abb2af75e0d136372f1f36e066c
Matti Ahinko <matti@ahinko.net>
2019-11-19 21:12:22 +0200

    Add fn call test

--- tests/ast/function.test.ts
+++ tests/ast/function.test.ts
@@ -1,5 +1,6 @@
-import { FunctionNode } from "../../src/ast/function"
+import { FunctionNode, FnCall } from "../../src/ast/function"
 import { createArguments } from "../../src/ast/system"
+import { StringNode } from "./valuenodes"
 
 describe("Create function node", () => {
   it("creates empty function", () => {
@@ -16,5 +17,24 @@ describe("Create function node", () => {
     })
     expect(fn.args.length).toBe(1)
     expect(fn.children.length).toBe(0)
+  })
+})
+
+describe("Create function call node", () => {
+  it("creates function call without params", () => {
+    const fnCall = new FnCall({
+      type: "fnCall",
+      children: [
+        new StringNode("test"),
+        {
+          type: "paramList",
+          children: [],
+          location: {}
+        }
+      ],
+      location: {}
+    })
+    expect(fnCall.params.length).toBe(0)
+    expect(fnCall.id).toBe("test")
   })
 })
