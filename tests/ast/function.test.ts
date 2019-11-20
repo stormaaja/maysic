6ee90d442b7b143fc9b69edcedbc73355df69bee
Matti Ahinko <matti@ahinko.net>
2019-11-20 20:55:54 +0200

    Add tests

--- tests/ast/function.test.ts
+++ tests/ast/function.test.ts
@@ -1,8 +1,9 @@
 import { FunctionNode, FnCall } from "../../src/ast/function"
 import { createArguments } from "../../src/ast/system"
 import { StringNode } from "./valuenodes"
+import { ASTEnvironment } from "../../src/ast/environment"
 
-function createFunctionNode(args: {[key: string]: string}) {
+function createFunctionNode(args: { [key: string]: string }) {
   return new FunctionNode({
     type: "function",
     children: [
@@ -36,11 +37,23 @@ describe("Create function node", () => {
     const fn = createFunctionNode({})
     expect(fn.args.length).toBe(0)
     expect(fn.children.length).toBe(0)
+    const env = { errors: [], symbols: {} }
+    fn.check(env, [])
+    expect(env.errors).toHaveLength(0)
+    expect(env.symbols).toStrictEqual({})
   })
   it("creates function with a arg", () => {
     const fn = createFunctionNode({ p1: "string" })
     expect(fn.args.length).toBe(1)
     expect(fn.children.length).toBe(0)
+  })
+  it("checks function with misplaced args", () => {
+    const fn = createFunctionNode({ p1: "string" })
+    const env: ASTEnvironment = { errors: [], symbols: {} }
+    fn.check(env, [])
+    expect(env.errors).toHaveLength(1)
+    expect(env.errors[0].error).toBe("tooFewArguments")
+    expect(env.symbols).toStrictEqual({})
   })
 })
 
@@ -52,10 +65,7 @@ describe("Create function call node", () => {
 
     const fn = createFunctionNode({})
 
-    const env = {
-      errors: [],
-      symbols: { test_void: fn }
-    }
+    const env = { errors: [], symbols: { test_void: fn } }
 
     fnCall.check(env)
     expect(env.errors.length).toBe(0)
